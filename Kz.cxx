894bf5f2d2c4433544755f27f414ea2e1113dbee
Vivien Mallet <Vivien.Mallet@inria.fr>
2003-06-10 08:06:11 +0000

    Placed AtmoData under the GNU General Public License.

--- Kz.cxx
+++ Kz.cxx
@@ -1,3 +1,23 @@
+// Copyright (C) 2003 Vivien Mallet
+//
+// This file is part of AtmoData library.
+// AtmoData library is a tool for data processing in atmospheric
+// sciences.
+// 
+// AtmoData is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// AtmoData is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License (file "license") for more details.
+//
+// For more information, please see the AtmoData home page:
+//     http://spacetown.free.fr/lib/atmodata
+
+
 #ifndef ATMODATA_FILE_KZ_CXX
 
 #include "Kz.hxx"

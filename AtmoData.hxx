4ae9dbec95baec3e24da5e8307a4306d775378c8
Vivien Mallet <Vivien.Mallet@inria.fr>
2003-07-30 15:52:22 +0000

    Removed functions 'to_num' because they are now defined in SeldonData.

--- AtmoData.hxx
+++ AtmoData.hxx
@@ -23,22 +23,6 @@
 #include "SeldonData.hxx"
 using namespace SeldonData;
 
-template <class T>
-void to_num(std::string s, T& num)
-{
-  std::istringstream str(s);
-  str >> num;
-}
-
-template <class T>
-T to_num(std::string s)
-{
-  T num;
-  std::istringstream str(s);
-  str >> num;
-  return num;
-}
-
 #include "Kz.cxx"
 #include "Winds.cxx"
 
